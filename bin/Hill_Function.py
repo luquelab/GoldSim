98cdd4ea6d3992c0ede454b4d12584fe35c6f1ab
Sergio <sergio@localhost.localdomain>
2022-12-30 06:00:22 -0800

    changes to code and documentation

--- bin/Hill_Function.py
+++ bin/Hill_Function.py
@@ -7,13 +7,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib 
 
-
+#Domain of edar and vector of edar values
+#--------------------------------------------
 eDAR_min=0.1
 eDAR_max=2
 
 eDAR_vec=np.arange(eDAR_min, eDAR_max, 0.01)
-print(eDAR_vec)
+#--------------------------------------------
 
+#K for conventional Hill function
 Ka=1
 
 exponents=[1,3,5,7,9]
@@ -21,16 +23,22 @@ Hills=[]
 Hills_Modified=[]
 
 for exponent in exponents:
+    #conventional hill
     Hill=[(eDAR)**exponent/(eDAR**exponent + Ka**exponent) for eDAR in eDAR_vec]
 
+    #Modified hill and parameters
     alpha=( (eDAR_max-eDAR_min)**exponent - 2*(1-eDAR_min)**exponent)/((eDAR_max-eDAR_min)**exponent - (1-eDAR_min)**exponent)
+    
     Ka_modified=(1-eDAR_min)*((2-alpha)**(1/exponent))
+    
     Hill_Modified=[(eDAR-eDAR_min)**exponent/(alpha*((eDAR-eDAR_min))**exponent + Ka_modified**exponent) for eDAR in eDAR_vec]
 
     Hills.append(Hill)
     Hills_Modified.append(Hill_Modified)
 
 
+#PLOTS
+#--------------------------------------------------------------
 #Fontsizes
 size_axis=9;size_ticks=9;
 
@@ -49,14 +57,12 @@ print(Hills_Modified[-2])
 
 width_line=1
 plt.axhline(y=0.5, color='k', linewidth=width_line,linestyle='dashed')
-plt.axvline(x=Ka, color='k', linewidth=width_line,linestyle='dashed') 
-
-
+plt.axvline(x=Ka, color='k', linewidth=width_line,linestyle='dashed')
 
 plt.ylabel('Hill function',fontsize=size_axis)
 plt.xlabel('e-DAR',fontsize=size_axis)
 plt.legend(loc='best',fontsize=size_ticks,frameon=False)
 
 
-
 plt.show()
+#--------------------------------------------------------------
